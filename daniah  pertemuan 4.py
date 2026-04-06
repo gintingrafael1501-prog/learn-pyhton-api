e8b94f2777d746db5a5754fc2b46505fe228e47c
gintingrafael1501-prog <gintingrafael1501@gmail.com>
2026-04-06 15:09:43 +0700

    Add files via upload

---
+++ daniah  pertemuan 4.py
@@ -0,0 +1,8 @@
+print ("kalkulasi zakat unpam")
+print("-------------------------------")
+input ("masukan nama:")
+input ("jenis zakat:")
+input ("jumlah garam:")
+input ("harga rp per gram")
+input ("presentase zakat ")
+input (" 
