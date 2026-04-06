6c3e065b169b991f9c28b5f6a2113794ae12d038
gintingrafael1501-prog <gintingrafael1501@gmail.com>
2026-04-06 16:01:09 +0700

    Delete daniah  pertemuan 4.py

--- daniah  pertemuan 4.py
+++
@@ -1,8 +0,0 @@
-print ("kalkulasi zakat unpam")
-print("-------------------------------")
-input ("masukan nama:")
-input ("jenis zakat:")
-input ("jumlah garam:")
-input ("harga rp per gram")
-input ("presentase zakat ")
-input (" 
